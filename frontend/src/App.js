eb461a89dfde47ca6fad3e1bec86e1d538ec98c6
Jayesh09871 <jayeshkakhani29@gmail.com>
2024-12-05 10:11:14 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import Signup from './components/Signup'
 
 export default function App() {
   return (
     <div>
       <Navbar/>
      <Home/>
+     <Signup/>
     </div>
   )
 }
